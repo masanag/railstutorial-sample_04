5575c50badb44fd598ebdd55b9967e1373dc8ca1
masanag <masaki.nagaishi@gmail.com>
2016-05-05 00:23:34 +0900

    done 10.3.3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 #
 module UsersHelper
-  def gravatar_for(user, options = {})
+  def gravatar_for(user, options = {size: 52})
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url =
       "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{options[:size]}"
