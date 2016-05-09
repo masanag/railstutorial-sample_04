19ca81d32bc4f2facccd64f3776afe382dc3c42c
masanag <masaki.nagaishi@gmail.com>
2016-05-10 08:57:49 +0900

    done 11.2.4

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -88,6 +88,18 @@ describe 'AuthenticationPages' do
         before { delete micropost_path(create(:micropost)) }
         specify { expect(response).to redirect_to(signin_path) }
       end
+
+      describe 'in the Relationships controller' do
+        describe 'submitting to the create action' do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
 
     describe 'as wrong user' do
