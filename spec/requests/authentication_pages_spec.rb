ccc41d28024324733ad38f3931c527e9f1803efc
masanag <masaki.nagaishi@gmail.com>
2016-05-10 08:25:06 +0900

    done 11.2.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -72,6 +72,16 @@ describe 'AuthenticationPages' do
           before { visit users_path }
           it { should have_title 'Sign in' }
         end
+
+        describe 'visiting the following page' do
+          before { visit following_user_path(user) }
+          it { should have_title('Following') }
+        end
+
+        describe 'visiting the followers page' do
+          before { visit followers_user_path(user) }
+          it { should have_title('Followers') }
+        end
       end
 
       describe 'submitting to the destroy action' do
